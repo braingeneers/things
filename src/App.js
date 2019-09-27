38e04d3bbca8a58fa9191769f8d4447bbd19f169
Rob Currie <rcurrie@ucsc.edu>
2019-09-26 18:34:43 -0700

    PubSub finnaly working from browser to device and back...oh IAM...

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import CameraIcon from '@material-ui/icons/Camera';
 import AWS from 'aws-sdk';
 
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify'
+import Amplify, { Auth, PubSub } from 'aws-amplify'
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
 
 let awsconfig = {
   Auth: {
@@ -51,11 +52,36 @@ class App extends Component {
       secretAccessKey: credentials.secretAccessKey,
       sessionToken: credentials.sessionToken
     })
-
-    this.setState({ iot: new AWS.Iot()})
+    const iot = new AWS.Iot({credentials: credentials})
+    this.setState({ iot: iot })
     this.setState({ things: await this.state.iot.listThings().promise() })
 
+    console.log(credentials.identityId)
+    iot.attachPrincipalPolicy({
+      policyName: 'default',
+      principal: credentials.identityId
+    }, (err, res) => { if (err) console.error(err); 
+    });
+
+    Amplify.addPluggable(new AWSIoTProvider({
+      aws_pubsub_region: awsconfig.Auth.region,
+      aws_pubsub_endpoint: 'wss://ahp00abmtph4i-ats.iot.us-west-2.amazonaws.com/mqtt'
+    }));
+
+    PubSub.subscribe('picroscope/#').subscribe({
+      next: data => console.log('Message received', data),
+      error: error => console.error(error),
+      close: () => console.log('Done'),
+    });
+
+
     this.setState({ loaded: true })
+  }
+
+  async publish(thing, action, message) {
+    console.log(`${thing}/${action}`, message)
+    await PubSub.publish(`${thing}/${action}`, { msg: message })
+
   }
 
   render() {
@@ -63,7 +89,8 @@ class App extends Component {
 
     return (
       <Container>
-        <Button onClick={() => Auth.signOut()}>Sign Out {this.state.user.getUsername()}</Button>
+        <Button variant="outlined"
+          onClick={() => Auth.signOut()}>Sign Out {this.state.user.getUsername()}</Button>
         <List>
         {this.state.things.things.map(thing =>
           <ListItem key={thing.thingArn}>
@@ -71,8 +98,10 @@ class App extends Component {
               <CameraIcon />
             </ListItemIcon>
             <ListItemText primary={thing.thingName} />
-            <Button>Start</Button>
-            <Button>Stop</Button>
+            <Button variant="outlined" color="primary" 
+              onClick={() => this.publish("picroscope", "start", "a message")}>Start</Button>
+            <Button variant="outlined" color="secondary" 
+              onClick={() => this.publish("picroscope", "stop", "a message")}>Stop</Button>
           </ListItem>
         )}
         </List>
